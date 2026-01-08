4f509ab16e8ba7f3e0896bef6bed48bf09268638
Toadie <toadie@toadie.de>
2026-01-08 22:12:14 +0100

    copied some files from branch alpha

--- README.md
+++ README.md
@@ -1,2 +1,155 @@
 # kvm-configurator
-# == alpha ==
+![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=flat&logo=go&logoColor=white) ![GitHub License](https://img.shields.io/github/license/mrtoadie/kvm-configurator) ![GitHub Downloads (all assets, all releases)](https://img.shields.io/github/downloads/mrtoadie/kvm-configurator/total)
+
+
+**kvm-configurator** is my first ![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=flat&logo=go&logoColor=white) project that goes beyond just playing around.
+
+**kvm-configurator** creates a **virtual machine** and registers it with qemu. In addition, the definition is saved as an **XML** file.
+
+The program can help you if you don't want to or can't use tools like virt-manager. Or if you don't feel like using commands like this:
+```bash
+virt-install \
+  --name guest1-rhel7 \
+  --memory 2048 \
+  --vcpus 2 \
+  --disk size=8 \
+  --cdrom /path/to/rhel7.iso \
+  --os-variant rhel7
+```
+
+![kvm-configurator](assets/kvm-configurator_01.png)
+
+## How to use?
+### Compiled version
+Download the two files oslist.yaml and kvm-config_x.x.
+Set kvm-config_x.x as an executable file:
+```bash
+chmod +x kvm-confi_x.x
+```
+and start the program with 
+```bash
+./kvm-config_x.x
+```
+### Alternatively - source file
+Use the file **kvm-config_x.x.go**
+```bash
+go run kvm_config.x.x.go 
+```
+
+### Tested under
+:white_check_mark: [Arch Linux](https://archlinux.org/)
+:white_check_mark: [GuideOS](https://guideos.de/) (Debian-based)
+:white_check_mark: [Ubuntu 25.04 & 25.10](https://ubuntu.com/)
+
+### Configuration
+#### YAML
+You can define default values for disk path and disk size in `oslist.yaml`.
+```yaml
+defaults: &default_vals               
+  disk_path: "/srv/vm-images"
+  disksize: 20
+```
+Using default values looks like this:
+```yaml
+- name: Arch Linux
+    id: archlinux
+    cpu: 2
+    ram: 2048
+    <<: *default_vals
+```
+You can overwrite the default values for your needs like this:
+```yaml
+- name: Debian 13
+    id: debian13
+    cpu: 2
+    ram: 3072
+    disksize: 20
+    disk_path: "/home/<user>/Downloads"
+```
+#### Sample XML output
+```xml
+<domain type="kvm">
+  <name>new-machine</name>
+  <uuid>1f13456f-87da-4901-ab9e-fc942fe1764f</uuid>
+  <metadata>
+    <libosinfo:libosinfo xmlns:libosinfo="http://libosinfo.org/xmlns/libvirt/domain/1.0">
+      <libosinfo:os id="http://ubuntu.com/ubuntu/20.04"/>
+    </libosinfo:libosinfo>
+  </metadata>
+  <memory>1048576</memory>
+  <currentMemory>1048576</currentMemory>
+  <vcpu>2</vcpu>
+  <os>
+    <type arch="x86_64" machine="q35">hvm</type>
+    <boot dev="hd"/>
+  </os>
+  <features>
+    <acpi/>
+    <apic/>
+    <vmport state="off"/>
+  </features>
+  <cpu mode="host-passthrough"/>
+  <clock offset="utc">
+    <timer name="rtc" tickpolicy="catchup"/>
+    <timer name="pit" tickpolicy="delay"/>
+    <timer name="hpet" present="no"/>
+  </clock>
+  <pm>
+    <suspend-to-mem enabled="no"/>
+    <suspend-to-disk enabled="no"/>
+  </pm>
+  <devices>
+    <emulator>/usr/bin/qemu-system-x86_64</emulator>
+    <disk type="file" device="disk">
+      <driver name="qemu" type="qcow2"/>
+      <source file="/run/media/vm/QEMU/new-machine.qcow2"/>
+      <target dev="vda" bus="virtio"/>
+    </disk>
+    <controller type="usb" model="qemu-xhci" ports="15"/>
+    <controller type="pci" model="pcie-root"/>
+    <controller type="pci" model="pcie-root-port"/>
+    <interface type="user">
+      <mac address="52:54:00:c2:70:03"/>
+      <model type="virtio"/>
+    </interface>
+    <console type="pty"/>
+    <channel type="unix">
+      <source mode="bind"/>
+      <target type="virtio" name="org.qemu.guest_agent.0"/>
+    </channel>
+    <channel type="spicevmc">
+      <target type="virtio" name="com.redhat.spice.0"/>
+    </channel>
+    <input type="tablet" bus="usb"/>
+    <graphics type="spice" port="-1" tlsPort="-1" autoport="yes">
+      <image compression="off"/>
+    </graphics>
+    <sound model="ich9"/>
+    <video>
+      <model type="virtio"/>
+    </video>
+    <redirdev bus="usb" type="spicevmc"/>
+    <memballoon model="virtio"/>
+    <rng model="virtio">
+      <backend model="random">/dev/urandom</backend>
+    </rng>
+  </devices>
+</domain>
+```
+
+## Release notes
+### Version 0.9.1 - Service release
+Some screen outputs are now color-coded.
+### Version 0.9
+Bug fixes related to the storage location for hard disk images
+### Version 0.8.5 - Support for YAML
+Operating systems are now read from a YAML file.
+The YAML can be easily customized by anyone according to their personal preferences.
+
+
+## Ideas & Roadmap
+- [X] logical, easy-to-understand menu
+- [ ] transform into TUI style
+- [ ] saving XMLs as templates and reediting
+- [ ] more & advanced VM parameters
+- [ ] web version for browser usage
---
+++ kvm-config_0.9.2.go
@@ -0,0 +1,381 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+
+	"gopkg.in/yaml.v3"
+)
+
+/* --------------------
+	Data structure
+-------------------- */
+type DomainConfig struct {
+	Name     string
+	MemMiB   int
+	VCPU     int
+	Disk     string
+	Disksize int
+	Network  string
+	ISO			 string
+}
+
+type distro struct {
+	Name     string `yaml:"name"`
+	ID       string `yaml:"id"`
+	CPU      int    `yaml:"cpu"`
+	RAM      int    `yaml:"ram"`
+	Disksize int    `yaml:"disksize"`
+	DiskPath string `yaml:"disk_path"`
+}
+
+type defaults struct {
+    DiskPath string
+		DiskSize int
+}
+
+/* --------------------
+	Global vars
+-------------------- */
+var globalDefaults defaults
+
+var (
+	osList          []distro
+	variantByName   map[string]string
+)
+
+/* --------------------
+	waiting for input
+-------------------- */
+func readLine(r *bufio.Reader, prompt string) (string, error) {
+	fmt.Print(prompt)
+	s, err := r.ReadString('\n')
+	return strings.TrimSpace(s), err
+}
+
+/* --------------------
+	Loading OS-List from yaml
+-------------------- */
+func loadOSList(p string) error {
+    b, err := ioutil.ReadFile(p)
+    if err != nil {
+        return fmt.Errorf("Could not read config: %w", err)
+    }
+
+    var root struct {
+        Defaults struct {
+            DiskPath string `yaml:"disk_path"`
+            DiskSize int    `yaml:"disksize"`
+        } `yaml:"defaults"`
+        OSList []distro `yaml:"oslist"`
+    }
+
+    if err = yaml.Unmarshal(b, &root); err != nil {
+        return fmt.Errorf("YAML could not be parsed: %w", err)
+    }
+
+    globalDefaults.DiskPath = root.Defaults.DiskPath
+    globalDefaults.DiskSize = root.Defaults.DiskSize
+    osList = root.OSList
+
+    variantByName = make(map[string]string, len(osList))
+    for _, d := range osList {
+        variantByName[d.Name] = d.ID
+    }
+    return nil
+}
+
+/* --------------------
+	Print the OS-List
+-------------------- */
+func chooseDistro(r *bufio.Reader) (distro, error) {
+	fmt.Println("\n=== Select an operating system ===")
+	sorted := append([]distro(nil), osList...)
+	sort.Slice(sorted, func(i, j int) bool {
+		return strings.ToLower(sorted[i].Name) < strings.ToLower(sorted[j].Name)
+	})
+	for i, d := range sorted {
+		fmt.Printf(" %2d) %s  (CPU:%d  RAM:%d MiB  Disk:%d GB)\n",
+			i+1, d.Name, d.CPU, d.RAM, d.Disksize)
+	}
+	line, err := readLine(r, "\nPlease enter a number (or press ENTER for default Arch Linux): ")
+	if err != nil {
+		return distro{}, err
+	}
+	idx := 1
+	if line != "" {
+		if i, e := strconv.Atoi(line); e == nil && i >= 1 && i <= len(sorted) {
+			idx = i
+		} else {
+			return distro{}, fmt.Errorf("\x1b[31mInvalid selection\x1b[0m")
+		}
+	}
+	return sorted[idx-1], nil
+}
+
+/* --------------------
+	Form – allows changes to the fields
+-------------------- */
+func (c *DomainConfig) edit(r *bufio.Reader) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0)
+	for {
+		fmt.Fprintln(w, "\n=== VM-Config ===\t")
+		fmt.Fprintf(w, "[1] Name:\t%s\t[default]\n", c.Name)
+		fmt.Fprintf(w, "[2] RAM (MiB):\t%d\t[default]\n", c.MemMiB)
+		fmt.Fprintf(w, "[3] vCPU:\t%d\t[default]\n", c.VCPU)
+		fmt.Fprintf(w, "[4] Disk-Path:\t%s\t[Enter path for virtual hdd]\n", c.Disk)
+		fmt.Fprintf(w, "[5] Disk-Size (GB):\t%d\t[default]\n", c.Disksize)
+		fmt.Fprintf(w, "[6] Network:\t%s\t[default]\n", c.Network)
+		w.Flush()
+
+		f, _ := readLine(r, "\nSelect or enter to continue: ")
+		if f == "" {
+			break
+		}
+		switch f {
+		case "1":
+			if v, _ := readLine(r, ">> New Name: "); v != "" {
+				c.Name = v
+			}
+		case "2":
+			if v, _ := readLine(r, ">> RAM (MiB): "); v != "" {
+				if i, e := strconv.Atoi(v); e == nil && i > 0 {
+					c.MemMiB = i
+				}
+			}
+		case "3":
+			if v, _ := readLine(r, ">> vCPU: "); v != "" {
+				if i, e := strconv.Atoi(v); e == nil && i > 0 {
+					c.VCPU = i
+				}
+			}
+		case "4":
+			if v, _ := readLine(r, ">> Disk path (empty = no disk): "); true {
+				c.Disk = v
+			}
+		case "5":
+			if v, _ := readLine(r, ">> Disksize (GB): "); v != "" {
+				if i, e := strconv.Atoi(v); e == nil && i > 0 {
+					c.Disksize = i
+				}
+			}
+		case "6":
+			if v, _ := readLine(r, ">> Network (comma-separated): "); true {
+				c.Network = v
+			}
+		default:
+			fmt.Println("\x1b[31mInvalid input!\x1b[0m")
+		}
+	}
+}
+
+// buildDiskArg create string for --disk.
+func buildDiskArg(cfg DomainConfig) (string, bool) {
+	// no disk path, no disk size :D
+	if strings.TrimSpace(cfg.Disk) == "" {
+		return "", false
+	}
+
+	p := strings.TrimSpace(cfg.Disk)
+
+	// if input is only path, append .qcow (<VM‑Name>.qcow2)
+	if !strings.HasSuffix(p, ".qcow2") && !strings.Contains(filepath.Base(p), ".") {
+		p = filepath.Join(p, cfg.Name+".qcow2")
+	} else if !strings.HasSuffix(p, ".qcow2") {
+		p = p + ".qcow2"
+	}
+
+	// defaul disk image file format
+	opts := []string{
+		fmt.Sprintf("path=%s", p),
+		"format=qcow2",
+	}
+
+	// use size i > 0
+	if cfg.Disksize > 0 {
+		opts = append([]string{fmt.Sprintf("size=%d", cfg.Disksize)}, opts...)
+	}
+
+	return strings.Join(opts, ","), true
+}
+
+func effectiveDiskPath(d distro) string {
+    if d.DiskPath != "" {
+        return d.DiskPath
+    }
+    return globalDefaults.DiskPath
+}
+
+// effectiveDiskSize
+func effectiveDiskSize(d distro) int {
+    if d.Disksize != 0 {
+        return d.Disksize
+    }
+    return globalDefaults.DiskSize
+}
+
+// diskSpec
+func diskSpec(cfg DomainConfig) (string, bool) {
+    // no path use ""
+    if strings.TrimSpace(cfg.Disk) != "" {
+        arg, _ := buildDiskArg(cfg) 
+        return arg, true
+    }
+
+    return "none", false
+}
+
+/* --------------------
+	Create VM
+-------------------- */
+func createVM(cfg DomainConfig, variant string) error {
+	r := bufio.NewReader(os.Stdin)
+
+	// ask for iso
+	iso, err := readLine(r, "Path to the installation ISO: ")
+	if err != nil {
+		return err
+	}
+
+	// build the vm with virt‑install
+	diskArg, haveRealDisk := diskSpec(cfg)
+
+  args := []string{
+    "--name", cfg.Name,
+    "--memory", strconv.Itoa(cfg.MemMiB),
+    "--vcpus", strconv.Itoa(cfg.VCPU),
+		"--cpu", "host-passthrough,+vmx", //Intel
+		//"--cpu", "host-passthrough,+svm", //AMD
+    "--os-variant", variant,
+    "--disk", diskArg,
+    "--cdrom", iso,
+    "--boot", "hd",
+    "--print-xml",
+  }
+  // logging
+  if haveRealDisk {
+  	fmt.Println("Using custom disk:", diskArg)
+  } else {
+  	fmt.Println("\x1b[34mNo custom disk – passing '--disk none'\x1b[0m")
+  }
+
+	cmd := exec.Command("virt-install", args...)
+	var out, errOut bytes.Buffer
+	cmd.Stdout, cmd.Stderr = &out, &errOut
+	if err = cmd.Run(); err != nil {
+		return fmt.Errorf("\x1b[31mvirt-install failed: %w – %s\x1b[0m", err, errOut.String())
+	}
+
+	// XML output
+	xml := out.Bytes()
+	fmt.Printf("XML created (%d Bytes).\n", len(xml))
+	if len(xml) > 0 {
+		fmt.Printf("First lines of the XML:\n%s\n", string(xml[:200]))
+	}
+	// save XML
+	xmlFile := cfg.Name + ".xml"
+	if err = os.WriteFile(xmlFile, xml, 0644); err != nil {
+		return fmt.Errorf("\x1b[31mcould not write XML: %w\x1b[0m", err)
+	}
+	abs, _ := filepath.Abs(xmlFile)
+	fmt.Printf("\x1b[32mXML definition saved under: %s\n\x1b[0m", abs)
+
+	// define VM
+	if err = exec.Command("virsh", "define", xmlFile).Run(); err != nil {
+		return fmt.Errorf("virsh define failed: %w", err)
+	}
+	fmt.Println("\x1b[32mVM successfully registered with libvirt/qemu (not yet started).\x1b[0m")
+	return nil
+}
+
+/* --------------------
+	Main workflow
+-------------------- */
+func runNewVMWorkflow() {
+	if err := loadOSList("oslist.yaml"); err != nil {
+		log.Fatalf("\x1b[31mError loading OS list: %v\x1b[0m", err)
+	}
+	r := bufio.NewReader(os.Stdin)
+
+	// choosing distribution
+	d, err := chooseDistro(r)
+	if err != nil {
+		log.Fatalf("\x1b[31mOS selection failed: %v\x1b[0m", err)
+	}
+	variant := variantByName[d.Name]
+
+	// create basic config from default vaules
+	cfg := DomainConfig{
+		Name:     d.Name,
+		MemMiB:   d.RAM,
+		VCPU:     d.CPU,
+		Disksize: effectiveDiskSize(d),
+    Disk:     effectiveDiskPath(d),
+		Network:  "default",
+	}
+
+	// manual editing
+	cfg.edit(r)
+
+	// VM summary
+	fmt.Println("\n=== SUMMARY ===")
+	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(w, "Name:\t%s\n", cfg.Name)
+	fmt.Fprintf(w, "RAM (MiB):\t%d\n", cfg.MemMiB)
+	fmt.Fprintf(w, "vCPU:\t%d\n", cfg.VCPU)
+	fmt.Fprintf(w, "Disk-Path:\t%s\n", cfg.Disk)
+	fmt.Fprintf(w, "Disksize:\t%d\n", cfg.Disksize)
+	fmt.Fprintf(w, "Network:\t%s\n", cfg.Network)
+	w.Flush()
+
+	// create vm
+	if err = createVM(cfg, variant); err != nil {
+		fmt.Fprintln(os.Stderr, "Error generating the VM:", err)
+		os.Exit(1)
+	}
+}
+
+/* --------------------
+	dummy function for later
+-------------------- */
+func dummyTest() { fmt.Println("Yay!") }
+
+/* --------------------
+	Mainmenu
+-------------------- */
+func main() {
+	for {
+		fmt.Println("\x1b[34m\n=== MAINMENU ===\x1b[0m")
+		fmt.Println("[1] New VM")
+		fmt.Println("[2] Dummy function")
+		fmt.Println("[0] Exit")
+		fmt.Print("Selection: ")
+
+		var choice int
+		if _, err := fmt.Scanln(&choice); err != nil {
+			fmt.Println("\x1b[31mPlease enter a valid number.\x1b[0m")
+			continue
+		}
+		switch choice {
+		case 0:
+			fmt.Println("Bye!")
+			return
+		case 1:
+			runNewVMWorkflow()
+		case 2:
+			dummyTest()
+		default:
+			fmt.Println("\x1b[31mInvalid selection!\x1b[0m")
+		}
+	}
+}
