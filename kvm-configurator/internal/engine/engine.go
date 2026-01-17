0b0a797cc5973686ba09de191124156340c8197c
Toadie <toadie@toadie.de>
2026-01-17 17:51:56 +0100

    update

--- kvm-configurator/internal/engine/engine.go
+++ kvm-configurator/internal/engine/engine.go
@@ -16,24 +16,6 @@ import (
 	"configurator/internal/ui"
 )
 
-func startSimpleProgress(msg string, stopChan <-chan struct{}) {
-	go func() {
-		chars := []rune{'⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷'}
-		i := 0
-		for {
-			select {
-			case <-stopChan:
-				fmt.Print("\r")
-				return
-			default:
-				fmt.Printf("\r%s %c ", msg, chars[i%len(chars)])
-				time.Sleep(100 * time.Millisecond)
-				i++
-			}
-	}
-}()
-}
-
 /* --------------------
 	CreateVM receives a fully‑filled DomainConfig, the os‑variant string
 	and the absolute path to the ISO file.
