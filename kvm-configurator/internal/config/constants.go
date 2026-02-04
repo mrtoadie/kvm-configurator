665bf9cca74dc978217d76bbda026886b9a1e64e
Toadie <toadie@toadie.de>
2026-02-04 21:51:17 +0100

    added constants.go for global use

---
+++ kvm-configurator/internal/config/constants.go
@@ -0,0 +1,9 @@
+// internal/constants.go
+// last modification: February 03 2026
+package config
+
+const (
+    CmdVirtInstall = "virt-install"
+    CmdVirsh       = "virsh"
+)
+var PrereqCommands = []string{CmdVirtInstall, CmdVirsh}
