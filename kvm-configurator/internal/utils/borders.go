345460d5dfbfe28d4931ed45ce088d8c7882c684
Toadie <toadie@toadie.de>
2026-02-06 19:33:16 +0100

    add utils/borders.go for border / box support

---
+++ kvm-configurator/internal/utils/borders.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Box(width int, lines []string) string {
+    maxLen := width
+    for _, l := range lines {
+        if len(l) > maxLen {
+            maxLen = len(l)
+        }
+    }
+
+    top    := "\033[34m╭\033[0m" + strings.Repeat("\033[34m─\033[0m", maxLen+2) + "\033[34m╮\033[0m"
+    bottom := "\033[34m╰\033[0m" + strings.Repeat("\033[34m─\033[0m", maxLen+2) + "\033[34m╯\033[0m"
+
+    var b strings.Builder
+    b.WriteString(top + "\n")
+    for _, l := range lines {
+        b.WriteString(fmt.Sprintf("\033[34m│\033[0m %-*s \033[34m│\033[0m\n", maxLen, l))
+    }
+    b.WriteString(bottom)
+    return b.String()
+}
+
+func BoxCenter(width int, lines []string) string {
+    maxLen := width
+    for _, l := range lines {
+        if len(l) > maxLen {
+            maxLen = len(l)
+        }
+    }
+
+    top    := "\033[34m╭\033[0m" + strings.Repeat("\033[34m─\033[0m", maxLen+2) + "\033[34m╮\033[0m"
+    bottom := "\033[34m╰\033[0m" + strings.Repeat("\033[34m─\033[0m", maxLen+2) + "\033[34m╯\033[0m"
+
+    var b strings.Builder
+    b.WriteString(top + "\n")
+    
+    for _, l := range lines {
+        // Zentrierung: Leerzeichen links + Text + Leerzeichen rechts
+        padding := (maxLen - len(l)) / 2
+        centered := strings.Repeat(" ", padding) + l + strings.Repeat(" ", maxLen-len(l)-padding)
+        b.WriteString(fmt.Sprintf("\033[34m│ %s │\033[0m\n", centered))
+    }
+    
+    b.WriteString(bottom)
+    return b.String()
+}
